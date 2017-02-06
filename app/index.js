13b43c0e721395f0b76950d81b8a913e284af7b7
Andy Mai <andrew.x.mai@gmail.com>
2017-02-05 16:23:52 -0800

    modal scrolls

--- app/index.js
+++ app/index.js
@@ -8,7 +8,6 @@ import { checkIfAuthed } from 'helpers/auth'
 import { routerReducer, syncHistoryWithStore } from 'react-router-redux'
 import * as reducers from 'redux/modules'
 import { hashHistory } from 'react-router'
-import 'sharedStyles/styles.css'
 
 const store = createStore(
   combineReducers({...reducers, routing: routerReducer}),
--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -38,7 +38,6 @@ const base = {
       {test: /\.css$/, loader: 'style!css?sourceMap&modules&localIdentName=[name]__[local]___[hash:base64:5]'},
       {test: /\.(png|woff|woff2|eot|ttf|svg)$/, loader: 'url-loader?limit=100000' },
       {test: /\.(jpg|eot(\?v=\d+\.\d+\.\d+)?)$/, loader: "file-loader" },
-
     ]
   },
   resolve: {
