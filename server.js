c8186d996cdf0bdf8eb7a83dace50e836e485a83
Andy Mai <andrew.x.mai@gmail.com>
2017-02-12 18:50:00 -0700

    change directories

--- server.js
+++ server.js
@@ -8,14 +8,15 @@ const app = express();
 // Setup logger
 //app.use(morgan(':remote-addr - :remote-user [:date[clf]] ":method :url HTTP/:http-version" :status :res[content-length] :response-time ms'));
 
-app.use(express.static(__dirname + '/dist'));
+//app.use(express.static(__dirname + '/dist'));
+app.use(express.static(path.resolve(__dirname, '..', 'dist'));
 
 // Always return the main index.html, so react-router render the route in the client
 app.get('/favicon.ico', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'dist', 'favicon.ico'));
+  res.sendFile(path.resolve(__dirname, '..', 'dist', 'favicon.ico'));
 });
 app.get('/*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'dist', 'index.html'));
+  res.sendFile(path.resolve(__dirname, '..', 'dist', 'index.html'));
 });
 
 const PORT = process.env.PORT || 9000;
