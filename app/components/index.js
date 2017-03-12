6af2dfbbfbd5968107ee2522eb74ef74cfbf6859
Andy Mai <andrew.x.mai@gmail.com>
2017-03-12 09:33:19 -0700

    remove unused components

--- app/components/index.js
+++ app/components/index.js
@@ -1,12 +1,7 @@
 export Home from './Home/Home'
 export Authenticate from './Authenticate/Authenticate'
 
-
-export NewProperty from './NewProperty/NewProperty'
-export Property from './Property/Property'
-
 export ExpenseTable from './Expenses/ExpenseTable'
-
 
 export AddContact from './AddContact/AddContact'
 export AddReminder from './AddReminder/AddReminder'
